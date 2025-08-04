63fc96f44b774fa9f6780486994756ba5e741902
maxassis <max.assis@ymail.com>
2025-08-04 17:59:28 -0300

    restore dashboard direct access

--- host/src/router/routes.tsx
+++ host/src/router/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../Pages/Login";
 import Dashboard from "../Pages/Dashboard";
 import InitialRedirect from "./InitialRedirect";
-// import ProtectedRoute from "./protectedRoutes";
+import ProtectedRoute from "./protectedRoutes";
 
 export const router = createBrowserRouter([
   {
@@ -11,7 +11,11 @@ export const router = createBrowserRouter([
   },
   {
     path: "/dashboard",
-    element: <Dashboard />,
+    element: (
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/",
